73e819e6770e8ee89acc7dcd2425ff7d063676a6
bannakonz <bannakon10@gmail.com>
2023-09-01 23:14:34 +0700

    add closing drawer

--- lib/screens/main_drawer.dart
+++ lib/screens/main_drawer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
-  const MainDrawer({super.key});
+  const MainDrawer({super.key, required this.onSelectScreen,});
+
+  final void Function(String identifier) onSelectScreen;
 
   @override
   Widget build(BuildContext context) {
@@ -57,7 +59,9 @@ class MainDrawer extends StatelessWidget {
                     fontSize: 24,
                   ),
             ),
-            onTap: (){},
+            onTap: (){
+              onSelectScreen('meals');
+            },
           ),
           ListTile(
             leading: Icon(
@@ -72,7 +76,9 @@ class MainDrawer extends StatelessWidget {
                 fontSize: 24,
               ),
             ),
-            onTap: (){},
+            onTap: (){
+              onSelectScreen('filters');
+            },
           ),
         ],
       ),
