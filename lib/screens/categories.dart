2318af3b22e5ca74d151062a694b11b4e38e9cf0
bannakonz <bannakon10@gmail.com>
2023-09-24 19:24:16 +0700

    fix padding

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -30,6 +30,7 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
+      padding: const EdgeInsets.all(24,),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         childAspectRatio: 3 / 2,
